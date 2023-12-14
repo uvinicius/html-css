e2c26d4313fb91114b00432433b437198d8f1b14
Vinícius da Silva <153554893+uvinicius@users.noreply.github.com>
2023-12-14 16:15:54 -0300

    Initial commit

---
+++ exercícios/ex8/teste.py
@@ -0,0 +1,8 @@
+nome= input("Digite o seu nome a seguir: ")
+print("\nBem-vindo", nome,", Meu querido!")
+
+x= int(input("Digite um número para nós: "))
+
+y= int(input("Digite outro número a seguir: "))
+
+print("O produto destes dois números é igual a ", x*y)
